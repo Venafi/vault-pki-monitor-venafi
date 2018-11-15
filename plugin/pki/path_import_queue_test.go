d9d255a957081c18d8da94865245ecb65db9b5cb
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2018-11-15 16:38:25 +0300

    Spliting tests

--- plugin/pki/path_import_queue_test.go
+++ plugin/pki/path_import_queue_test.go
@@ -138,7 +138,7 @@ func TestBackend_PathImportToTPP(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	time.Sleep(5 * time.Second)
+	time.Sleep(15 * time.Second)
 
 	//retrieve imported certificate
 	//res.Certificates[0].CertificateRequestId != "\\VED\\Policy\\devops\\vcert\\renx3.venafi.example.com"
@@ -154,12 +154,142 @@ func TestBackend_PathImportToTPP(t *testing.T) {
 	}
 
 	req := &certificate.Request{}
-	req.PickupID = "\\VED\\Policy\\devops\\vcert\\"+singleCN
+	req.PickupID = "\\\\VED\\\\Policy\\\\devops\\\\vcert\\\\"+singleCN
 	cl, err := vcert.NewClient(tppConfig)
 	if err != nil {
 		t.Fatalf("could not connect to endpoint: %s", err)
 	}
-	cl.RetrieveCertificate(req)
+	pcc, err := cl.RetrieveCertificate(req)
+	if err != nil {
+		t.Fatalf("could not retrieve certificate using requestId %s: %s", req.PickupID, err)
+	}
+	log.Println("Printing pcc")
+	pp(pcc)
+
+
+	//list import queue
+	resp, err = b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.ListOperation,
+		Path:      "import-queue/",
+		Storage:   storage,
+	})
+	if resp != nil && resp.IsError() {
+		t.Fatalf("failed to list certs, %#v", resp)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	keys := resp.Data["keys"]
+	log.Printf("Import queue list is:\n %v", keys)
+	time.Sleep(30 * time.Second)
+
+}
+
+func TestBackend_PathImportToTPPMultipleCerts(t *testing.T) {
+	rand := randSeq(5)
+	domain := "example.com"
+
+	// create the backend
+	config := logical.TestBackendConfig()
+	storage := &logical.InmemStorage{}
+	config.StorageView = storage
+
+	b := Backend(config)
+	err := b.Setup(context.Background(), config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// generate root
+	rootData := map[string]interface{}{
+		"common_name": domain,
+		"ttl":         "6h",
+	}
+
+	resp, err := b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "root/generate/internal",
+		Storage:   storage,
+		Data:      rootData,
+	})
+	if resp != nil && resp.IsError() {
+		t.Fatalf("failed to generate root, %#v", resp)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// config urls
+	urlsData := map[string]interface{}{
+		"issuing_certificates":    "http://127.0.0.1:8200/v1/pki/ca",
+		"crl_distribution_points": "http://127.0.0.1:8200/v1/pki/crl",
+	}
+
+	resp, err = b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "config/urls",
+		Storage:   storage,
+		Data:      urlsData,
+	})
+	if resp != nil && resp.IsError() {
+		t.Fatalf("failed to config urls, %#v", resp)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create a role entry
+	roleData := map[string]interface{}{
+		"allowed_domains":    domain,
+		"allow_subdomains":   "true",
+		"max_ttl":            "4h",
+		"allow_bare_domains": true,
+		"generate_lease":     true,
+		"tpp_import":         true,
+		"tpp_url":            os.Getenv("TPPURL"),
+		"tpp_user":           os.Getenv("TPPUSER"),
+		"tpp_password":       os.Getenv("TPPPASSWORD"),
+		"zone":               os.Getenv("TPPZONE"),
+		"trust_bundle_file":  os.Getenv("TRUST_BUNDLE"),
+		"tpp_import_timeout": 2,
+		"tpp_import_workers": 5,
+	}
+
+	resp, err = b.HandleRequest(context.Background(), &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "roles/test-import",
+		Storage:   storage,
+		Data:      roleData,
+	})
+	if resp != nil && resp.IsError() {
+		t.Fatalf("failed to create a role, %#v", resp)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//issue some certs
+	i := 1
+	for i < 10 {
+		randCN := rand + "-import." + domain
+		certData := map[string]interface{}{
+			"common_name": randCN,
+		}
+		resp, err = b.HandleRequest(context.Background(), &logical.Request{
+			Operation: logical.UpdateOperation,
+			Path:      "issue/test-import",
+			Storage:   storage,
+			Data:      certData,
+		})
+		if resp != nil && resp.IsError() {
+			t.Fatalf("failed to issue a cert, %#v", resp)
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		i = i + 1
+	}
 
 
 	//list import queue
