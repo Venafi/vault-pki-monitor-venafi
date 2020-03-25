c594bd38941b26e3c929b651835595b9fdad1b57
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2020-03-25 19:22:59 +0300

    merge entry

--- plugin/pki/role_venafi_sync.go
+++ plugin/pki/role_venafi_sync.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/hashicorp/vault/sdk/logical"
-	"log"
 )
 
 func (b *backend) roleVenafiSync(ctx context.Context, req *logical.Request) (response *logical.Response, err error) {
@@ -13,7 +12,6 @@ func (b *backend) roleVenafiSync(ctx context.Context, req *logical.Request) (res
 	//name
 	//sync zone name\id
 	//sync endpoint
-
 
 	//For each role do:
 	roleName := "venafi-sync-role"
@@ -29,59 +27,48 @@ func (b *backend) roleVenafiSync(ctx context.Context, req *logical.Request) (res
 	}
 	//Set role parameters
 	entryRewrite := &roleEntry{
-		OU:                            zone.SubjectOURegexes,
-		Organization:                  []string{"Venafi"},
-		Country:                       []string{zone.Country},
-		Locality:                      []string{"Salt Lake"},
-		Province:                      []string{"Venafi"},
-		StreetAddress:                 []string{"Venafi"},
-		PostalCode:                    []string{"122333344"},
-
+		OU:            zone.SubjectOURegexes,
+		Organization:  []string{"Venafi"},
+		Country:       []string{zone.Country},
+		Locality:      []string{"Salt Lake"},
+		Province:      []string{"Venafi"},
+		StreetAddress: []string{"Venafi"},
+		PostalCode:    []string{"122333344"},
 	}
 	//	Read previous role parameters
-     entryOriginal := &roleEntry{
-		 AllowLocalhost:                true,
-		 AllowedDomains:                []string{"venafi.com"},
-		 AllowBareDomains:              true,
-		 AllowSubdomains:               true,
-		 AllowGlobDomains:              true,
-		 AllowAnyName:                  true,
-		 EnforceHostnames:              true,
+	entryOriginal := &roleEntry{
+		AllowLocalhost:   true,
+		AllowedDomains:   []string{"venafi.com"},
+		AllowBareDomains: true,
+		AllowSubdomains:  true,
+		AllowGlobDomains: true,
+		AllowAnyName:     true,
+		EnforceHostnames: true,
 
-		 OU:                            []string{"DevOps-old"},
-		 Organization:                  []string{"Venafi-old"},
-		 Country:                       []string{"US-old"},
-		 Locality:                      []string{"Salt Lake-old"},
-		 Province:                      []string{"Venafi-old"},
-		 StreetAddress:                 []string{"Venafi-old"},
-		 PostalCode:                    []string{"122333344-old"},
-
-	 }
-    //  Make new entry
-    //implement merge
-    log.Printf("merging entry\n %v and \n%v", entryRewrite, entryOriginal)
-
-    entry := &roleEntry{
-		AllowLocalhost:                true,
-		AllowedDomains:                []string{"venafi.com"},
-		AllowBareDomains:              true,
-		AllowSubdomains:               true,
-		AllowGlobDomains:              true,
-		AllowAnyName:                  true,
-		EnforceHostnames:              true,
-
-		OU:                            []string{"DevOps"},
-		Organization:                  []string{"Venafi"},
-		Country:                       []string{"US"},
-		Locality:                      []string{"Salt Lake"},
-		Province:                      []string{"Venafi"},
-		StreetAddress:                 []string{"Venafi"},
-		PostalCode:                    []string{"122333344"},
-
+		OU:            []string{"DevOps-old"},
+		Organization:  []string{"Venafi-old"},
+		Country:       []string{"US-old"},
+		Locality:      []string{"Salt Lake-old"},
+		Province:      []string{"Venafi-old"},
+		StreetAddress: []string{"Venafi-old"},
+		PostalCode:    []string{"122333344-old"},
 	}
-    // Put new entry
+	//  Make new entry
+	//implement merge
+	mergedEntry := entryOriginal
+	mergedEntry.OU = entryRewrite.OU
+	mergedEntry.Organization = entryRewrite.Organization
+	mergedEntry.Country = entryRewrite.Country
+	mergedEntry.Locality = entryRewrite.Locality
+	mergedEntry.Province = entryRewrite.Province
+	mergedEntry.StreetAddress = entryRewrite.StreetAddress
+	mergedEntry.PostalCode = entryRewrite.PostalCode
+
+
+
+	// Put new entry
 	// Store it
-	jsonEntry, err := logical.StorageEntryJSON("role/"+roleName, entry)
+	jsonEntry, err := logical.StorageEntryJSON("role/"+roleName, entryRewrite)
 	if err != nil {
 		return nil, err
 	}
