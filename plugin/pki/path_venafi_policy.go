5475b995f6a01319472bef1d8584bdc49cef0a76
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2020-04-03 14:06:19 +0300

    regsiter policy auto refresh

--- plugin/pki/path_venafi_policy.go
+++ plugin/pki/path_venafi_policy.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/vault/sdk/logical"
 	"log"
 	"strings"
+	"time"
 )
 
 const venafiPolicyPath = "venafi-policy/"
@@ -77,6 +78,11 @@ https://golang.org/pkg/crypto/x509/#ExtKeyUsage
 -- simply drop the "ExtKeyUsage" part of the name.
 Also you can use constants from this module (like 1, 5,8) direct or use OIDs (like 1.3.6.1.5.5.7.3.4)`,
 			},
+			"auto_refresh": {
+				Type:        framework.TypeBool,
+				Default:     true,
+				Description: `Automatically update policy from Venafi`,
+			},
 		},
 		Callbacks: map[logical.Operation]framework.OperationFunc{
 			logical.UpdateOperation: b.pathUpdateVenafiPolicy,
@@ -123,6 +129,40 @@ func pathVenafiPolicyList(b *backend) *framework.Path {
 	return ret
 }
 
+func (b *backend) refreshVenafiPolicyContentRegister(storage logical.Storage, conf *logical.BackendConfig) {
+	log.Println("registering policy sync controller")
+	b.taskStorage.register("policy-refresh-controller", func() {
+		err := b.refreshVenafiPolicyContent(storage, conf)
+		if err != nil {
+			log.Printf("%s", err)
+		}
+	}, 1, time.Second*15)
+}
+
+func (b *backend) refreshVenafiPolicyContent(storage logical.Storage, conf *logical.BackendConfig) (err error) {
+	ctx := context.Background()
+	policies := []string{"1", "2", "3"}
+	for _, name := range policies {
+		//name := data.Get("name").(string)
+
+		policy, err := b.getPolicyFromVenafi(ctx, storage, name)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		policyEntry, err := savePolicyEntry(policy, name, ctx, storage)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		respData := formPolicyRespData(*policyEntry)
+		log.Println(respData)
+	}
+	return nil
+}
+
 func (b *backend) pathReadVenafiPolicyContent(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	name := data.Get("name").(string)
 	log.Printf("Trying to read policy for config %s", name)
@@ -156,12 +196,12 @@ func (b *backend) pathReadVenafiPolicyContent(ctx context.Context, req *logical.
 func (b *backend) pathUpdateVenafiPolicyContent(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	name := data.Get("name").(string)
 
-	policy, err := b.getPolicyFromVenafi(ctx, req, name)
+	policy, err := b.getPolicyFromVenafi(ctx, req.Storage, name)
 	if err != nil {
 		return nil, err
 	}
 
-	policyEntry, err := savePolicyEntry(policy, name, ctx, req)
+	policyEntry, err := savePolicyEntry(policy, name, ctx, req.Storage)
 	if err != nil {
 		return nil, err
 	}
@@ -186,6 +226,7 @@ func (b *backend) pathUpdateVenafiPolicy(ctx context.Context, req *logical.Reque
 			TPPUser:         data.Get("tpp_user").(string),
 			TrustBundleFile: data.Get("trust_bundle_file").(string),
 		},
+		AutoRefresh: data.Get("auto_refresh").(bool),
 	}
 	unparsedKeyUsage := data.Get("ext_key_usage").([]string)
 	configEntry.ExtKeyUsage, err = parseExtKeyUsageParameter(unparsedKeyUsage)
@@ -203,11 +244,11 @@ func (b *backend) pathUpdateVenafiPolicy(ctx context.Context, req *logical.Reque
 		return nil, err
 	}
 	log.Printf("Geting policy from zone %s", data.Get("zone").(string))
-	policy, err := b.getPolicyFromVenafi(ctx, req, name)
+	policy, err := b.getPolicyFromVenafi(ctx, req.Storage, name)
 	if err != nil {
 		return nil, err
 	}
-	policyEntry, err := savePolicyEntry(policy, name, ctx, req)
+	policyEntry, err := savePolicyEntry(policy, name, ctx, req.Storage)
 	if err != nil {
 		return nil, err
 	}
@@ -217,9 +258,10 @@ func (b *backend) pathUpdateVenafiPolicy(ctx context.Context, req *logical.Reque
 	return &logical.Response{
 		Data: respData,
 	}, nil
+
 }
 
-func savePolicyEntry(policy *endpoint.Policy, name string, ctx context.Context, req *logical.Request) (policyEntry *venafiPolicyEntry, err error) {
+func savePolicyEntry(policy *endpoint.Policy, name string, ctx context.Context, storage logical.Storage) (policyEntry *venafiPolicyEntry, err error) {
 
 	//Form policy entry for storage
 	policyEntry = &venafiPolicyEntry{
@@ -244,7 +286,7 @@ func savePolicyEntry(policy *endpoint.Policy, name string, ctx context.Context, 
 	if err != nil {
 		return nil, err
 	}
-	if err := req.Storage.Put(ctx, jsonEntry); err != nil {
+	if err := storage.Put(ctx, jsonEntry); err != nil {
 		return nil, err
 	}
 
@@ -287,9 +329,9 @@ func formPolicyRespData(policy venafiPolicyEntry) (respData map[string]interface
 	}
 }
 
-func (b *backend) getPolicyFromVenafi(ctx context.Context, req *logical.Request, policyConfig string) (policy *endpoint.Policy, err error) {
+func (b *backend) getPolicyFromVenafi(ctx context.Context, storage logical.Storage, policyConfig string) (policy *endpoint.Policy, err error) {
 	log.Printf("Creating Venafi client")
-	cl, err := b.ClientVenafi(ctx, req.Storage, policyConfig, "policy")
+	cl, err := b.ClientVenafi(ctx, storage, policyConfig, "policy")
 	if err != nil {
 		return
 	}
@@ -313,7 +355,7 @@ func (b *backend) pathReadVenafiPolicy(ctx context.Context, req *logical.Request
 	log.Printf("Trying to read policy for config %s", name)
 
 	if len(name) == 0 {
-		return logical.ErrorResponse("Non config specified or wrong config path name"), nil
+		return logical.ErrorResponse("No config specified or wrong config path name"), nil
 	}
 
 	entry, err := req.Storage.Get(ctx, venafiPolicyPath+name)
@@ -588,6 +630,7 @@ func (b *backend) getPolicyConfig(ctx context.Context, s logical.Storage, n stri
 type venafiPolicyConfigEntry struct {
 	venafiConnectionConfig
 	ExtKeyUsage []x509.ExtKeyUsage `json:"ext_key_usage"`
+	AutoRefresh bool               `json:"auto_refresh"`
 }
 
 type venafiPolicyEntry struct {
