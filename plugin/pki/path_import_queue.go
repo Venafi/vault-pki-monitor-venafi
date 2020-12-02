03f9e038eae1f3b60a0ffdd7199d7d245402c278
angelmoo <angel.moo@venafi.com>
2020-12-01 18:32:44 -0600

    correcting typos and converting validation to status code 401 to be an and instead of or condition plus revert back validation for previous tpp versions

--- plugin/pki/path_import_queue.go
+++ plugin/pki/path_import_queue.go
@@ -281,7 +281,8 @@ func (b *backend) processImportToTPP(job Job) string {
 			//validate if the error is related to a expired access token, at this moment the only way can validate this is using the error message
 			//and verify if that message describes errors related to expired access token.
 			code := getStatusCode(msg)
-			if code == HTTP_UNAUTHORIZED || regex.MatchString(msg) {
+			if code == HTTP_UNAUTHORIZED &&
+				( (strings.Contains(msg, "\"error\":\"expired_token\"") && strings.Contains(msg, "\"error_description\":\"Access token expired\"")) || regex.MatchString(msg) )  {
 
 				cfg, err := b.getConfig(job.ctx, job.storage, job.policyName)
 
--- plugin/pki/path_venafi_policy_sync.go
+++ plugin/pki/path_venafi_policy_sync.go
@@ -282,7 +282,8 @@ func (b *backend) getVenafiPolicyParams(ctx context.Context, storage logical.Sto
 		//validate if the error is related to a expired accces token, at this moment the only way can validate this is using the error message
 		//and verify if that message describes errors related to expired access token.
 		code := getStatusCode(msg)
-		if code == HTTP_UNAUTHORIZED || regex.MatchString(msg) {
+		if code == HTTP_UNAUTHORIZED &&
+			( (strings.Contains(msg, "\"error\":\"expired_token\"") && strings.Contains(msg, "\"error_description\":\"Access token expired\"")) || regex.MatchString(msg) )  	{
 
 			cfg, err := b.getConfig(ctx, &storage, policyConfig)
 
