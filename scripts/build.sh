204243de19f0443a4f44b65aa778c7f27aab0bbc
ryan.treat <ryan.treat@venafi.com>
2020-12-09 06:44:12 +0000

    Drop build number from release names; no more darwin86; add checksum to zips; actually build optional versions

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,10 +8,10 @@ BUILD_MODE=$4
 VERSION=$5
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [ ${BUILD_MODE}=="strict" ]; then
+if [ ${BUILD_MODE} == "strict" ]; then
     echo "Changing  venafiPolicyDenyAll to true"
     sed -i 's/const venafiPolicyDenyAll =.*/const venafiPolicyDenyAll = true/' plugin/pki/vcert.go
-elif [ ${BUILD_MODE}=="optional" ]; then
+elif [ ${BUILD_MODE} == "optional" ]; then
     echo "Changing  venafiPolicyDenyAll to false"
     sed -i 's/const venafiPolicyDenyAll =.*/const venafiPolicyDenyAll = false/' plugin/pki/vcert.go
 else
@@ -23,14 +23,21 @@ mkdir -p ${CURRENT_DIR}/../${DIST_DIR}
 
 for os in linux darwin windows; do
     for arch in 386 amd64; do
-        if echo ${arch}|grep --quiet 386; then
-            binary_name="${PLUGIN_DIR}/${os}86/${PLUGIN_NAME}_${BUILD_MODE}"
-            archive_name="${CURRENT_DIR}/../${DIST_DIR}/${PLUGIN_NAME}_${VERSION}_${os}86_${BUILD_MODE}"
-        else
-            binary_name="${PLUGIN_DIR}/${os}/${PLUGIN_NAME}_${BUILD_MODE}"
-            archive_name="${CURRENT_DIR}/../${DIST_DIR}/${PLUGIN_NAME}_${VERSION}_${os}_${BUILD_MODE}"
+        if [ "${os}" == "darwin" ] && [ "${arch}" == "386" ]; then continue; fi
+        case ${arch} in
+            386)
+                binary_name="${PLUGIN_DIR}/${os}86/${PLUGIN_NAME}"
+                archive_name="${CURRENT_DIR}/../${DIST_DIR}/${PLUGIN_NAME}_${VERSION}_${os}86_${BUILD_MODE}"
+                ;;
+            *)
+                binary_name="${PLUGIN_DIR}/${os}/${PLUGIN_NAME}"
+                archive_name="${CURRENT_DIR}/../${DIST_DIR}/${PLUGIN_NAME}_${VERSION}_${os}_${BUILD_MODE}"
+                ;;
+        esac
+        if [ ${BUILD_MODE} != "strict" ]; then
+            binary_name="${binary_name}_${BUILD_MODE}"
         fi
-        if echo ${os}|grep --quiet  windows; then
+        if [ "${os}" == "windows" ]; then
             binary_name="${binary_name}.exe"
         fi
         echo "Building plugin binary ${binary_name} for ${os}-${arch}"
@@ -38,10 +45,7 @@ for os in linux darwin windows; do
         chmod +x ${binary_name}
         echo "Archiving binary into ${archive_name}.zip"
         SHA256=$(sha256sum ${binary_name}| head -c 64)
-        echo "${SHA256} ${PLUGIN_NAME}_${BUILD_MODE}" > ${archive_name}.SHA256SUM
-        zip -j "${archive_name}.zip" "${binary_name}"
+        echo "${SHA256}" > ${binary_name}.SHA256SUM
+        zip -j "${archive_name}.zip" "${binary_name}" "${binary_name}.SHA256SUM"
     done
 done
-
-echo "Checksums for binaries:"
-cat ${CURRENT_DIR}/../${DIST_DIR}/*.SHA256SUM
