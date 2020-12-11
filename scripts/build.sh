c65e912e107b7c60489971ecb1eaa56ca2d6ef76
ryan.treat <ryan.treat@venafi.com>
2020-12-11 20:22:38 +0000

    Minor simplification

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,23 +23,24 @@ mkdir -p ${CURRENT_DIR}/../${DIST_DIR}
 
 for os in linux darwin windows; do
     for arch in 386 amd64; do
+        # 32-bit macOS build is now retired since support was dropped in Go 1.15
         if [ "${os}" == "darwin" ] && [ "${arch}" == "386" ]; then continue; fi
-        case ${arch} in
-            386)
-                binary_name="${PLUGIN_DIR}/${os}86/${PLUGIN_NAME}"
-                archive_name="${CURRENT_DIR}/../${DIST_DIR}/${PLUGIN_NAME}_${VERSION}_${os}86_${BUILD_MODE}"
-                ;;
-            *)
-                binary_name="${PLUGIN_DIR}/${os}/${PLUGIN_NAME}"
-                archive_name="${CURRENT_DIR}/../${DIST_DIR}/${PLUGIN_NAME}_${VERSION}_${os}_${BUILD_MODE}"
-                ;;
+
+        case "${arch}" in
+            386) EXT="86" ;;
+            *) EXT="" ;;
         esac
+        binary_name="${PLUGIN_DIR}/${os}${EXT}/${PLUGIN_NAME}"
+        archive_name="${CURRENT_DIR}/../${DIST_DIR}/${PLUGIN_NAME}_${VERSION}_${os}${EXT}_${BUILD_MODE}"
+
         if [ ${BUILD_MODE} != "strict" ]; then
             binary_name="${binary_name}_${BUILD_MODE}"
         fi
+
         if [ "${os}" == "windows" ]; then
             binary_name="${binary_name}.exe"
         fi
+
         echo "Building plugin binary ${binary_name} for ${os}-${arch}"
         env CGO_ENABLED=0 GOOS=${os}  GOARCH=${arch} go build -ldflags '-s -w -extldflags "-static"' -a -o ${binary_name} || exit 1
         chmod +x ${binary_name}
